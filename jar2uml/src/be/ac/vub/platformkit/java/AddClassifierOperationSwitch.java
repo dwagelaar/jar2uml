a5f46ab64874ee9b96c5bc9e587401fa84d511a8
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2007-03-22 17:27:57 +0000

    Fork JarToUML

---
+++ jar2uml/src/be/ac/vub/platformkit/java/AddClassifierOperationSwitch.java
@@ -0,0 +1,56 @@
+package be.ac.vub.platformkit.java;
+
+import junit.framework.Assert;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Interface;
+import org.eclipse.uml2.uml.util.UMLSwitch;
+
+public class AddClassifierOperationSwitch extends UMLSwitch {
+	
+	private String operationName = null;
+	private EList argumentTypes = null;
+
+	public EList getArgumentTypes() {
+		return argumentTypes;
+	}
+
+	public void setArgumentTypes(EList argumentTypes) {
+		this.argumentTypes = argumentTypes;
+	}
+
+	public String getOperationName() {
+		return operationName;
+	}
+
+	public void setOperationName(String operationName) {
+		this.operationName = operationName;
+	}
+
+	public Object caseClass(Class object) {
+		String name = getOperationName();
+		EList args = getArgumentTypes();
+		Assert.assertNotNull(name);
+		Assert.assertNotNull(args);
+		return object.createOwnedOperation(name, toUMLArgNames(args), args);
+	}
+
+	public Object caseInterface(Interface object) {
+		String name = getOperationName();
+		EList args = getArgumentTypes();
+		Assert.assertNotNull(name);
+		Assert.assertNotNull(args);
+		return object.createOwnedOperation(name, toUMLArgNames(args), args);
+	}
+
+	private static EList toUMLArgNames(EList args) {
+		EList umlArgNames = new BasicEList();
+		for (int i = 0; i < args.size(); i++) {
+			umlArgNames.add("arg" + i);
+		}
+		return umlArgNames;
+	}
+	
+}
---
+++ jar2uml/src/be/ac/vub/platformkit/java/AddClassifierPropertySwitch.java
@@ -0,0 +1,45 @@
+package be.ac.vub.platformkit.java;
+
+import junit.framework.Assert;
+
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Interface;
+import org.eclipse.uml2.uml.Type;
+import org.eclipse.uml2.uml.util.UMLSwitch;
+
+public class AddClassifierPropertySwitch extends UMLSwitch {
+
+	private String propertyName = null;
+	private Type propertyType = null;
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+	public Type getPropertyType() {
+		return propertyType;
+	}
+
+	public void setPropertyType(Type propertyType) {
+		this.propertyType = propertyType;
+	}
+
+	public Object caseClass(Class umlClass) {
+		String name = getPropertyName();
+		Type type = getPropertyType();
+		Assert.assertNotNull(name);
+		return umlClass.createOwnedAttribute(name, type);
+	}
+
+	public Object caseInterface(Interface umlIface) {
+		String name = getPropertyName();
+		Type type = getPropertyType();
+		Assert.assertNotNull(name);
+		return umlIface.createOwnedAttribute(name, type);
+	}
+
+}
