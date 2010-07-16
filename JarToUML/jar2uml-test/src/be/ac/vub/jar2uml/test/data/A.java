4a988eaf5c44b941f6b918ee54e1baba76ff433a
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2010-07-16 13:05:11 +0000

    Test updated to cover: bug 56: Container types of referenced types are removed during dependency model extraction, resulting in dangling references https://soft.vub.ac.be/bugzilla/show_bug.cgi?id=56

---
+++ JarToUML/jar2uml-test/src/be/ac/vub/jar2uml/test/data/A.java
@@ -0,0 +1,24 @@
+package be.ac.vub.jar2uml.test.data;
+
+public class A {
+	
+	public class AA {
+		
+	}
+
+	private B.BB bbField;
+
+	/**
+	 * @return the bbField
+	 */
+	public B.BB getBbField() {
+		return bbField;
+	}
+
+	/**
+	 * @param bbField the bbField to set
+	 */
+	public void setBbField(B.BB bbField) {
+		this.bbField = bbField;
+	}
+}
