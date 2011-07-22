2b02f644d00cf1db28fb289df7da81b11d6bfe72
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2011-07-22 07:53:38 +0000

    Update copyright.

--- JarToUML/jar2uml/src/be/ac/vub/jar2uml/MergeModel.java
+++ JarToUML/jar2uml/src/be/ac/vub/jar2uml/MergeModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2010 Dennis Wagelaar, Vrije Universiteit Brussel.
+ * Copyright (c) 2007-2011 Dennis Wagelaar, Vrije Universiteit Brussel.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
